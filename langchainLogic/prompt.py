e62e211a82e8c18e7ce1c9e35151785e8e71f329
Alperen Dagli <alperen.dagli43@gmail.com>
2023-08-23 16:59:17 +0200

    connecting everything up

--- langchainLogic/prompt.py
+++ langchainLogic/prompt.py
@@ -7,35 +7,48 @@ from langchain.chains import ConversationalRetrievalChain
 
 from langchainLogic.indexer import indexRepo
 
-def promptLangchain(repoURL,promptBody):
 
-    os.environ['OPENAI_API_KEY'] = ""
+def promptLangchain(repoURL, promptBody):
+    print("Starting promptLangchain function...")
 
+    os.environ["OPENAI_API_KEY"] = os.environ["API_KEY"]
 
+    print("Initializing embeddings...")
     embeddings = OpenAIEmbeddings(disallowed_special=())
-    print("indexing repo")
+
+    print("Indexing repository...")
     db = DeepLake(indexRepo(repoURL), read_only=True, embedding_function=embeddings)
 
+    print("Configuring retriever...")
     retriever = db.as_retriever()
-    retriever.search_kwargs['distance_metric'] = 'cos'
-    retriever.search_kwargs['fetch_k'] = 100
-    retriever.search_kwargs['maximal_marginal_relevance'] = True
-    retriever.search_kwargs['k'] = 10
-    print("loaded retriever")
+    retriever.search_kwargs["distance_metric"] = "cos"
+    retriever.search_kwargs["fetch_k"] = 100
+    retriever.search_kwargs["maximal_marginal_relevance"] = True
+    retriever.search_kwargs["k"] = 10
+
+    print("Loading chat model...")
     model = ChatOpenAI(model="gpt-3.5-turbo-0613")
-    qa = ConversationalRetrievalChain.from_llm(model,retriever=retriever) # add verbose = True to see the full convo
+    qa = ConversationalRetrievalChain.from_llm(
+        model, retriever=retriever
+    )  # add verbose = True to see the full convo
 
+    print("Preparing questions...")
+    questions = [
+        "Resolve the issue in the given text, which is delimited by XML tags. Print only the code for the solution. The issue is enclosed within: <Issue> "
+        + promptBody
+        + "</Issue>"
+    ]
 
-    #questions = ["Resolve the following issue in the given text. Provide the solution for the issue in a git.patch file format. The issue is delimited in XML Tags. <Issue> " + promptBody + "</Issue>"]
-    questions = ["What is the primary programming language used in this repository?"]
+    print("Starting conversation retrieval...")
     chat_history = []
     for question in questions:
         result = qa({"question": question, "chat_history": chat_history})
-        ##chat_history.append((question, result['answer']))
         print(f"-> **Question**: {question} \n")
         print(f"**Answer**: {result['answer']} \n")
 
-        #append result into textfile in generatedDocs folder
-        with open("/result/result.txt", "a") as myfile:
-            myfile.write(result['answer']+"\n")
+        print("Appending result to text file...")
+        with open("../result/result.txt", "a") as myfile:
+            myfile.write(result["answer"] + "\n")
             myfile.close()
+
+    print("promptLangchain function completed.")
