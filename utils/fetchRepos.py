395199f10dc97fd9d09be3a4e5fba7c56f1251a3
Jimmy Neitzert <jimmy.neitzert@gmail.com>
2023-09-22 00:52:40 +0200

    :adhesive_bandage: fix checkout not pulling remotes

--- utils/fetchRepos.py
+++ utils/fetchRepos.py
@@ -11,7 +11,7 @@ def getRepo(repoURL):
     if not os.path.exists(repoFolder + name[-1]):
         print("Cloning repo " + name[-1] + " to repos folder")
         Repo.clone_from(repoURL, repoFolder + name[-1])
-    #checkoutBranch(repoFolder + name[-1])
+    checkoutBranch(repoFolder + name[-1])
     return str(repoFolder + name[-1])
 
 
@@ -25,24 +25,26 @@ def checkoutBranch(repo_path):
         # Fetch the latest updates from the remote repository
         repo.remotes.origin.fetch()
 
-        branches = [str(branch) for branch in repo.branches]
+        local_branches = [str(branch) for branch in repo.branches]
+        remote_branches = [str(branch) for branch in repo.remotes.origin.refs]
 
-        if not branches:
+        if not local_branches and not remote_branches:
             print("No branches found in the repository.")
             return
 
         print("Available branches:")
-        for i, branch in enumerate(branches):
+        for i, branch in enumerate(local_branches + remote_branches):
             print(f"{i + 1}. {branch}")
 
         branch_choice = input("Enter the number of the branch you want to checkout: ")
 
         try:
             branch_choice = int(branch_choice)
-            if 1 <= branch_choice <= len(branches):
-                selected_branch = branches[branch_choice - 1]
+            if 1 <= branch_choice <= len(local_branches + remote_branches):
+                selected_branch = local_branches[branch_choice - 1] if branch_choice <= len(local_branches) else \
+                remote_branches[branch_choice - 1 - len(local_branches)]
                 print(f"Checking out branch '{selected_branch}'...")
-                repo.git.checkout(selected_branch)
+                repo.git.checkout(selected_branch)  # Add force=True to force checkout
                 print(f"Successfully checked out branch '{selected_branch}'.")
             else:
                 print("Invalid branch choice.")
